879bdb3ec611327757866a9b15a7da06a404a796
Rich1927 <richfeynman007@gmail.com>
2025-04-29 23:07:18 +0300

    ux
     Результаты урока scrapy

---
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class DivannewparsSpider(scrapy.Spider):
+    name = "divannewpars"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://divan.ru/category/divany-i-kresla"]
+# start_urls - это та ссылка, от которой начинается парсинг
+    def parse(self, response):
+        pass
